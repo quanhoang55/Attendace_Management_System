2e94916309486ef9570c1ca6859ac4ce164f5003
quanhoang55 <quanhoang1026@gmail.com>
2026-06-15 14:15:25 +0700

    models/ updated

--- src/models/schedule.py
+++ src/models/schedule.py
@@ -1,19 +1,37 @@
 #==========================================================================
 # IMPORTS & CONFIGURATION
 #==========================================================================
-
+from enum import IntEnum
+from dataclasses import dataclass
+from typing import Optional
 #==========================================================================
 # CLASSES / DATA STRUCTURE: Weekday-List, Period-List, Room-List
 #==========================================================================
 
+class WeekdayList(IntEnum):
+    MODAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+    SUNDAY = 7
+
+class Room(IntEnum):
+    NO1 = 1
+    NO2 = 2
+    NO3 = 3
+    NO4 = 4
+    NO5 = 5
+
 # Uncertain, Change it Later
-weekday_list = []
-period_list = []
-room_list = []
+# period_list = []
 
 #==========================================================================
 # CLASSES / DATA STRUCTURE: Schedule
 #==========================================================================
+@dataclass
 class Schedule:
-    def __init__(self,):
-        pass
+    weekday: WeekdayList
+    period: int
+    room: Room
--- src/models/student.py
+++ src/models/student.py
@@ -2,17 +2,13 @@
 # IMPORTS & CONFIGURATION
 #==========================================================================
 import uuid
+from dataclasses import dataclass, field
 
 #==========================================================================
 # CLASSES / DATA STRUCTURE: __BLUEPRINT__
 #==========================================================================
 
+@dataclass
 class Student:
-    def __init__(self, full_name: str):
-        """Student Class
-
-        Args:
-            full_name (str): student's full name
-        """        
-        self.stuent_id = str(uuid.uuid4())
-        self.full_name = full_name
+    student_id: str = field(default_factory=lambda: str(uuid.uuid4()))
+    full_name: str
